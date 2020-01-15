a4ae3d1e1ede07b5475cf99d51de72a63e9bd2d0
serrudojc <49172116+serrudojc@users.noreply.github.com>
2020-01-15 12:08:20 -0300

    dudas en funciones de calculo de edad ejercicio 1
    No se pq en alguno casos tengo que castear con Number y en otros no, en funciones de calculo de edad.
    Seguir con el punto bonus.

--- tareas/clase-6/jc/ejercicio1.js
+++ tareas/clase-6/jc/ejercicio1.js
@@ -20,10 +20,11 @@ function calculaMayorEdad(array){
     return mayorEdad;
 }
 
+//Duda: aca tuve que castear a Number pq si no, no funcionaba. Pq en calculaMayoredad no hace falta?
 function calculaMenorEdad(array){
     let menorEdad = array[0].value;
     for(let i=0; i<array.length; i++){
-        if(menorEdad > array[i].value){
+        if(menorEdad > Number(array[i].value)){
             menorEdad = array[i].value;
         }
     }
@@ -33,9 +34,9 @@ function calculaMenorEdad(array){
 function calculaPromedioEdad(array){
     let promedioEdad = 0;
     for(let i=0; i<array.length; i++){
-        promedioEdad +=array[0].value;
+        promedioEdad = promedioEdad + Number(array[i].value);
     }
-    return promedioEdad/array.length;
+    return (promedioEdad/array.length).toFixed(2);
 }
 
 const botonNumFamiliares = document.querySelector('#btn-1');
@@ -75,15 +76,28 @@ botonNumFamiliares.onclick = function(){
     const calcular = document.querySelector('#btn-2');
     calcular.onclick = function(){
         const edades = document.querySelectorAll('.edades');
+
+        const nodoDivResultados = document.querySelector('#resultados');
     
         let mayorEdad = calculaMayorEdad(edades);
         let menorEdad = calculaMenorEdad(edades);
         let promedioEdad = calculaPromedioEdad(edades);
 
-        console.log('mayor edad es '+mayorEdad);
-        for(let i=0; i<edades.length; i++){
-            console.log(edades[i].value);
-        }
+        let nuevoParrafoMayor = document.createElement('p');
+        let nuevoParrafoMenor = document.createElement('p');
+        let nuevoParrafoProm = document.createElement('p');
+
+        let textoMayorEdad = document.createTextNode('La persona de mayor edad tiene '+mayorEdad+' años.');
+        let textoMenorEdad = document.createTextNode('La persona de menor edad tiene '+menorEdad+' años.');
+        let textoPromedioEdad = document.createTextNode('Promedio de edad son '+promedioEdad+' años.');
+
+        nuevoParrafoMayor.appendChild(textoMayorEdad);
+        nodoDivResultados.appendChild(nuevoParrafoMayor);
+        nuevoParrafoMenor.appendChild(textoMenorEdad);
+        nodoDivResultados.appendChild(nuevoParrafoMenor);
+        nuevoParrafoProm.appendChild(textoPromedioEdad);
+        nodoDivResultados.appendChild(nuevoParrafoProm);
+
         return false;
     }
     
